4f45d049ff340e2f0aa372768c1b9e0eca24933d
yokoiakinori <yokoiakinori@icloud.com>
2021-03-07 18:02:13 +0900

    blade修正

--- server/laravel/resources/views/index.blade.php
+++ server/laravel/resources/views/index.blade.php
@@ -6,7 +6,7 @@
   <title>{{ config('app.name') }}</title>
 
   <!-- Scripts -->
-  <script src="{{asset('js/app.js')}}" defer></script>
+  <script src="{{mix('js/app.js')}}" defer></script>
 
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
